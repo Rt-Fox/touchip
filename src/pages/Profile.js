f9e69f5762318de2d95b80964f1f17cff2c5741f
Ruslan <54994813+Rt-Fox@users.noreply.github.com>
2021-03-02 02:43:14 +0300

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,52 +5,22 @@ import Fields from "../components/Fields";
 import {observer} from "mobx-react-lite";
 import Modal from "../components/modal";
 import {getId} from "../http/userAPI";
+import Nickname from "../components/Nickname";
+import Avatar from "../components/Avatar";
 
 const Profile = observer(() => {
     const [card, setCard] = useState({})
-    const [element, setElement] = useState('btn btn-secondary submit')
-    const [name, setName] = useState(card.displayed_name);
     const {id} = useParams()
 
     useEffect(() => {
         retrieveCard(id).then(data => setCard(data))
     }, [id])
 
-    useEffect(() => {
-        setName(card.displayed_name)
-    }, [card.displayed_name])
-
-
-    function handleChange(event) {
-        setName(event.target.value);
-        if (card.displayed_name !== event.target.value) {
-            setElement('btn btn-secondary submit visibility')
-        } else {
-            setElement('btn btn-secondary submit')
-        }
-    }
-
-    async function handleSubmit() {
-        let id = await getId();
-        await partialUpdateCard(id, {displayed_name: name})
-        setElement('btn btn-secondary submit')
-    }
-
-
     return (
         <div className="container mt-5">
             <div className="row d-flex flex-column align-items-center">
-                <div className="element-photo">
-                    <img src={card.photo} alt=""/>
-                </div>
-                <h2 className="Title">
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={handleChange}
-                    />
-                    <button className={element} type="submit" onClick={handleSubmit}>Ok</button>
-                </h2>
+                <Avatar props={card} />
+                <Nickname props={card} />
                 <div className="col-4">
                     <Fields key={card.id} props={card.fields}/>
                 </div>
@@ -59,5 +29,3 @@ const Profile = observer(() => {
         </div>
     );
 });
-
-export default Profile;
